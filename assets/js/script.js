f226db31b436200f5c6a63bf857b7f09e6dc3b43
sian504 <>
2023-10-17 09:25:21 +0000

    Updated JS to display data on the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,18 @@
-// Initial References
-let result = $("#result");
-let searchBtn = $("#search-btn");
-let url = "https://tasty.p.rapidapi.com/recipes/auto-complete";
-
-searchBtn.on("click", () => {
-    let userInp = $("#user-inp").val();
-    if (userInp.length === 0) {
-        result.html(`<h3>Input Field Cannot Be Empty</h3>`);
-    } else {
-        $.ajax({
-            url: url,
-            type: "GET",
-            data: { prefix: userInp },
-            headers: {
-                "x-rapidapi-key": "459369c516mshfa57b82cae3e3b3p1b41a1jsn79b5694f966a",
-                "x-rapidapi-host": "tasty.p.rapidapi.com",
-            },
-            success: function (response) {
-                if (response && response.length > 0) {
-                    let recipe = response[0];
-                    if (recipe && recipe.thumbnail_url) {
-                        console.log(recipe);
-
-                        result.html(`
-                        <img src="${recipe.thumbnail_url}">
-                        <div class="details">
-                            <h2>${recipe.name}</h2>
-                            <h4>Cuisine: ${recipe.cuisine}</h4>
-                        </div>
-                        <div id="recipe">
-                            <button id="hide-recipe">X</button>
-                            <pre id="instructions">${recipe.instructions}</pre>
-                        </div>
-                        <button id="show-recipe">View Recipe</button>
-                        `);
-                    } else {
-                        result.html(`<h3>Recipe data is missing or invalid</h3>`);
-                    }
-                } else {
-                    result.html(`<h3>No recipe found for the given ingredient</h3>`);
-                }
-            }
-        });
+const settings = {
+    async: true,
+    crossDomain: true,
+    url: 'https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=chicken%20soup',
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '459369c516mshfa57b82cae3e3b3p1b41a1jsn79b5694f966a',
+        'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
     }
+};
+
+$.ajax(settings).done(function (response) {
+    // Get a reference to the container element
+    const container = $("#result");
+
+    // Display the response in the container
+    container.html(JSON.stringify(response, null, 2));
 });
